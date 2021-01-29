c5a28da2854d4676ea0c7007f66d2b00c4c7149a
Vangelis Foufikos <baggelisfoufikos@yahoo.com>
2021-01-29 20:43:09 +0200

    finish with database

---
+++ database/Dump20210129/iPollute_recycle.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 5.7.32, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: iPollute
+-- ------------------------------------------------------
+-- Server version	5.7.32-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `recycle`
+--
+
+DROP TABLE IF EXISTS `recycle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recycle` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(45) DEFAULT NULL,
+  `cost` float DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recycle`
+--
+
+LOCK TABLES `recycle` WRITE;
+/*!40000 ALTER TABLE `recycle` DISABLE KEYS */;
+INSERT INTO `recycle` VALUES (1,'Glass Bottle',0.4),(2,'Mixed Plastics',4),(3,'Aluminium Can',8.5),(4,'Aluminium foil',8.4),(5,'Mixed Cans',3.5),(6,'Aerosols',3.5),(7,'Textiles & Footwear',3.3),(8,'Matresses',1.2),(9,'Fridges &freezers',0.8),(10,'Steel Cans',0.8),(11,'Car tyres',0.6),(12,'Paper',0.5),(13,'Composite food and drink cartoons',0.5),(14,'Wood (inc.Chipboard)',0.5),(15,'Light bulbs',0.7),(17,'Card',0.2);
+/*!40000 ALTER TABLE `recycle` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-01-29 16:40:26
